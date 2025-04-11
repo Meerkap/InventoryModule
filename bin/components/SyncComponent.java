84a91fb141c146a47bf2374b5f3a470d0a97a475
Meerkap <meerkapdev@gmail.com>
2025-04-11 15:52:55 +0100

    subscribe to other events

--- bin/components/SyncComponent.java
+++ bin/components/SyncComponent.java
@@ -4,6 +4,7 @@ package me.meerkap.rpgmarketplace.bin.components;
 import me.meerkap.rpgmarketplace.bin.controller.ViewRegistry;
 import me.meerkap.rpgmarketplace.bin.dispatcher.EventDispatcher;
 import me.meerkap.rpgmarketplace.bin.dispatcher.events.InventoryUpdateEvent;
+import me.meerkap.rpgmarketplace.bin.dispatcher.events.ItemRemovedEvent;
 import me.meerkap.rpgmarketplace.bin.module.PaginatedModel;
 
 /**
@@ -11,11 +12,16 @@ import me.meerkap.rpgmarketplace.bin.module.PaginatedModel;
  */
 public class SyncComponent {
     public static void registerGlobalSync(PaginatedModel model) {
-        System.out.println("Sincronización global registrada para el modelo " + model.getUniqueId());
+        // Suscripción para actualizaciones generales del inventario.
         EventDispatcher.subscribe(InventoryUpdateEvent.class, (event, view) -> {
-            if (event.getSourceModel().equals(model)) {
-                ViewRegistry.refreshAllViews(model.getUniqueId());
-            }
+            ViewRegistry.refreshAllViews(model.getUniqueId());
         });
+
+        // Suscripción para la eliminación de ítems en el inventario.
+        EventDispatcher.subscribe(ItemRemovedEvent.class, (event, view) -> {
+            ViewRegistry.refreshAllViews(model.getUniqueId());
+        });
+
+        System.out.println("Sincronización global registrada para el modelo " + model.getUniqueId());
     }
 }
